605b8a537c9564961adf0369219f319e3558ead0
ArintonAkos <43067524+ArintonAkos@users.noreply.github.com>
2023-01-08 12:21:25 +0200

    First commit

---
+++ Assets/InnerDriveStudios/RubikCube/Scripts/Utils/Prefabs.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets.InnerDriveStudios.RubikCube.Scripts.Utils
+{
+    internal static class Prefabs
+    {
+        public static GameObject DiceEmptyCorner = Resources.Load("") as GameObject;
+    }
+}
